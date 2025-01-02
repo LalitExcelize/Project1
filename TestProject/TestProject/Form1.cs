f26896b8374bac2cdd4851e30fd6b4b9d7498ad0
LalitExcelize <lalit@excelize.com>
2025-01-02 16:23:32 +0530

    New Project Added
    New Project Folder Added In Clone Repository

---
+++ TestProject/TestProject/Form1.cs
@@ -0,0 +1,17 @@
+namespace TestProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test Project Is Running");
+            MessageBox.Show("BranchOne");
+            MessageBox.Show("BranchOne");
+        }
+    }
+}
